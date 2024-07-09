09c6de4c784c2edf53ba240112099380ecdb5335
Your GitHub Username <Your GitHub Email>
2024-07-09 12:31:09 -0600

    Added api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,16 @@
-// import Image from "next/image";
-import styles from "./page.module.css";
-import { Container,Box,Image } from "@chakra-ui/react";
-import Navbar from './navbar/navbar';
-import NavbarTwo from "./navbar/navbarTwo";
-import SemiNav from './navbar/semiNav';
-import HomePageBody from "./components/homePageBody";
-import CreateAccount from './createAccount/createAccount/page';
-import GeneralDisplay from "./createAccount/generalDisplay";
-// import PersonaDetail from "./createAccount/PersonalDetails/page";
-import Login from './createAccount/Login/page';
-import VerifyMail from "./components/CreateAccount_Components/VerifyMail";
-import CreatePin from "./components/CreateAccount_Components/CreatePin";
-// import Confirmpin from "./createAccount/confirmPin/page";
-import Bvn from "./Kyc/Bvn/Bvn";
-import Nin from "./Kyc/Nin/Nin";
-import Kkyc from "./Kyc/Kyc/page";
-import Sell from "./components/drawer/Sell/Sell";
-import AddBank from "./components/drawer/Sell/addBank";
-import Add from "./components/drawer/Sell/nnn";
-import ResetPassword from "./createAccount/ResetPassword/page";
-// import EnterVerification from "./createAccount/EnterVerification/page";
-import Success from "./components/CreateAccount_Components/success";
-import HomepageForIncompletekyc from "./incompleteKyc/HomepageForIncompletekyc";
- import Notification from "./incompleteKyc/Notification";
-import NavbarTwo_nokyc from "./navbar/navbarTwo_nokyc";
+
+'use client'
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import React from 'react'
+import NavbarTwo from "./navbar/navbarTwo";
+import HomePageBody from "./components/homePageBody";
 
 
 export default function Home() {
-   // eslint-disable-next-line react-hooks/rules-of-hooks
+   
    const router = useRouter();
-   // eslint-disable-next-line react-hooks/rules-of-hooks
+
    useEffect(() => {
      const auth = localStorage.getItem("stk-apk");
      if (!auth) {
