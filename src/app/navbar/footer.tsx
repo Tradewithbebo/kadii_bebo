14a79bb6780befef024243ac34d8679f985a4d06
Your GitHub Username <Your GitHub Email>
2024-10-01 09:12:27 -0600

    Added changes

--- src/app/navbar/footer.tsx
+++ src/app/navbar/footer.tsx
@@ -1,16 +1,22 @@
-import { HStack } from '@chakra-ui/react'
-import React from 'react'
-import navItems from './navitem'
-import NavLink, { NavLinkMobile } from './navLink'
-import navItemsM from './navItemMobile'
+import { HStack } from '@chakra-ui/react';
+import React from 'react';
+import navItems from './navitem';
+import NavLink, { NavLinkMobile } from './navLink';
+import navItemsM from './navItemMobile';
 
 export default function Footer() {
   return (
-    <HStack gap={['80px','130px','']}  justifyContent={'center'} display={'flex'} py={['15px']} >
-    {navItemsM.map((item) => (
-      <NavLinkMobile  key={item.title} items={item} />
-    ))}
-  </HStack>
-  )
+    <HStack
+      gap={['50px', '40px', '80px']} // Adjusting gap for mobile, tablet, and desktop
+      justifyContent={'center'}
+      display={'flex'}
+      py={['10px', '15px', '20px']} // Padding for mobile, tablet, and desktop
+     
+      alignItems={'center'}
+    >
+      {navItemsM.map((item) => (
+        <NavLinkMobile key={item.title} items={item} />
+      ))}
+    </HStack>
+  );
 }
-
